ff459cfffdeade1045fd8537700d8145c6c4657a
emariew <emariewestfall@gmail.com>
2017-04-20 15:09:25 -0700

    webpack.config update to remove minification warning on production

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require("webpack");
+
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/OrganGrinder.jsx",
@@ -17,6 +20,25 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+          // Don't beautify output (enable for neater output)
+          beautify: false,
+
+          // Eliminate comments
+          comments: false,
+
+          // Compression specific options
+          compress: {
+            warnings: false,
+
+            // Drop `console` statements
+            drop_console: true
+          },
+        })
+  ],
   devtool: 'source-map',
   resolve: {
     extensions: ["", ".js", ".jsx"]
